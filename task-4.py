40fb27df3391b1215c87bfbc316ddded837037a6
AndreyU76 <43966723+AndreyU76@users.noreply.github.com>
2019-01-28 12:57:24 +0300

    Create task-4.py

---
+++ task-4.py
@@ -0,0 +1,31 @@
+import sys
+import platform
+
+n = int(input("введи данные n: "))
+a = 0
+for i in range(1,n+1):
+    a += i
+m = n * (n + 1) // 2
+#print(a)
+#print(m)
+
+
+def show_size(x, level=0):
+    print ('\t' * level, x.__class__, sys.getsizeof(x), x)
+    if hasattr(x, '__iter__'):
+        if hasattr(x, 'items'):
+            for xx in x.items():
+                show_size(xx, level + 1)
+        else:
+            for xx in x:
+                show_size(xx, level + 1)
+
+
+#print(sys.getsizeof(n))
+print(f'Общий объём памяти = {sys.getsizeof(a)} байт')
+print(f'Общий объём памяти = {sys.getsizeof(n)} байт')
+print(f'Общий объём памяти = {sys.getsizeof(m)} байт')
+print(f'Общий объём памяти = {show_size(n)} байт')
+
+print (sys.platform)
+print (sys.version)
